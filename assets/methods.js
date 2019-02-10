67a38b4da0b7fe729160311d491160c97900dd4e
MIMrxn <Fractal@Marvins-MacBook-Pro.local>
2019-02-09 20:15:51 -0800

    added first generation population

--- assets/methods.js
+++ assets/methods.js
@@ -1,16 +1,16 @@
 //	Methods for Project 1: Cella Rule 150
 
 //	Rule 150 Configuration cells
-let configs = [
-	{ config: [1, 1, 1], next: 1 },
-	{ config: [1, 1, 0], next: 0 },
-	{ config: [1, 0, 1], next: 0 },
-	{ config: [1, 0, 0], next: 1 },
-	{ config: [0, 1, 1], next: 0 },
-	{ config: [0, 1, 0], next: 1 },
-	{ config: [0, 0, 1], next: 1 },
-	{ config: [0, 0, 0], next: 0 },
-];
+var configs = { 
+	"1, 1, 1": 1,
+	"1, 1, 0": 0,
+	"1, 0, 1": 0,
+	"1, 0, 0": 1,
+	"0, 1, 1": 0,
+	"0, 1, 0": 1,
+	"0, 0, 1": 1,
+	"0, 0, 0": 0
+};
 
 //	Initial setup process
 //	Generates first generation
@@ -20,43 +20,63 @@ function setup( ctx, cells ) {
 	ctx.scale(4, 4);
   	ctx.fillRect(0, 0, canvas.width, canvas.height);
   	draw_grid( ctx, 1, 50, 'black', 'blue' );
-  	draw_rect( ctx, "red", "black");
+  	//draw_rect( ctx, "red", "black");
+  	draw_rect( ctx, "black", 0, 199);
 }
 
 //	Populates cells by finding next generation cells
 function populateCells( ctx, cells, curr_row ) {
 	var curr_row = 0;
+	var nextGenRow = [];
 	
 	//	Split current generation row into subcells of size 3
 	//	Currently testing so using some function instead of forEach
 	//	forEach will be used in final version
 	cells.some(function(row) {
-		document.write("".concat(curr_row + ": " + row));
+		//document.write(curr_row + ": " + row);
 
 		var curr_col = 0;
 		row.some(function(col) {
-			var splitRow = [];
+			var subcells = [];
+			
+			//	Handle corner cases as empty when no cell is available
 			if ( curr_col === 0 ) {
-				splitRow.push(0);
-				splitRow.push(row[curr_col]);
-				splitRow.push(row[curr_col+1]);
+				subcells.push(0);
+				subcells.push(row[curr_col]);
+				subcells.push(row[curr_col+1]);
 			} else if ( curr_col === 399 ) {
-				splitRow.push(row[curr_col-1]);
-				splitRow.push(row[curr_col]);
-				splitRow.push(0)
+				subcells.push(row[curr_col-1]);
+				subcells.push(row[curr_col]);
+				subcells.push(0)
 			} else {
-				splitRow.push(row[curr_col-1]);
-				splitRow.push(row[curr_col]);
-				splitRow.push(row[curr_col+1])
+				subcells.push(row[curr_col-1]);
+				subcells.push(row[curr_col]);
+				subcells.push(row[curr_col+1])
 			}
-			document.write(curr_col + ": " + splitRow + "\n");
+
+			//	Get next generation value
+			var nextGenValue = getNextGenValue(subcells);
+			nextGenRow.push(nextGenValue);
+			if(nextGenValue === 1) {
+				draw_rect(ctx, "black", curr_row+1, curr_col);
+			}
+
 			curr_col++;
-			return curr_col === 201;
+			//return curr_col === 201;
 		});
 		
 		curr_row++;
+		// Stop after first generation
 		return curr_row === 1;
 	});
+}
+
+function getNextGenValue( subcells ) {
+	for(var key in configs) {
+		if(key === subcells.join(", ")) {
+			return configs[key];
+		}
+	}
 }
 
 // Draw stuff
@@ -64,17 +84,13 @@ function populateCells( ctx, cells, curr_row ) {
 // ------------------------------------------------------------
 
 // FUN. Draw filled rect.
-function draw_rect( ctx, stroke, fill ) 
+//function draw_rect( ctx, stroke, fill ) 
+function draw_rect( ctx, fill, row, col ) 
 {
-    stroke = stroke || 'lightgrey';
     fill = fill || 'dimgrey';
     ctx.save( );
-    //ctx.strokeStyle = stroke;
     ctx.fillStyle = fill;
-    //ctx.lineWidth = 5;
-    //ctx.rect(75, 50, canvas.width - 150, canvas.height - 100);
-    ctx.rect(199, 0, 1, 1);
-    //ctx.stroke();
+    ctx.rect(col, row, 1, 1);
     ctx.fill();
     ctx.restore( );
 }
